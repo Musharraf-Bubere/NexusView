338e3e92e4418e73904472e0133d7e18e6c9992b
Musharraf-Bubere <musharrafbubere007@gmail.com>
2026-03-11 15:37:21 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools 
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.1"
+
+REPO_NAME = "NexusView"
+AUTHOR_USER_NAME = "Musharraf-Bubere"
+AUTHOR_EMAIL = "musharrafbubere007@gmail.com"
+SRC_REPO = "nexusview"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
